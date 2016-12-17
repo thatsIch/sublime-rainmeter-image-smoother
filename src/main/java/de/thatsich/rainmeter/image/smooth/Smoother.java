8bec723c1295ca8d29509846271961e2e8bcf944
thatsIch <thatsIch@mail.de>
2016-12-17 10:42:28 +0100

    Handle multiple images at once

--- src/main/java/de/thatsich/rainmeter/image/smooth/Smoother.java
+++ src/main/java/de/thatsich/rainmeter/image/smooth/Smoother.java
@@ -17,44 +17,45 @@ import static org.bytedeco.javacpp.opencv_imgproc.cvtColor;
  */
 public class Smoother {
 	public static void main(String[] args) {
-		if (args.length < 1 || args.length > 1) {
-			System.err.println("Requires one argument: path to image");
+		if (args.length < 1) {
+			System.err.println("Requires at least one argument: <path to image>+");
 			return;
 		}
 
-		final String filename = args[0];
-		final File inputFile = new File(filename);
-		final Mat input = Loader.loadOrExit(inputFile, IMREAD_UNCHANGED);
-		if (input.channels() == 3) {
-			cvtColor(input, input, CV_RGB2RGBA, 4);
+		for (final String filename : args) {
+			final File inputFile = new File(filename);
+			final Mat input = Loader.loadOrExit(inputFile, IMREAD_UNCHANGED);
+			if (input.channels() == 3) {
+				cvtColor(input, input, CV_RGB2RGBA, 4);
+			}
+
+			// copy data over to create a new Black Shadow
+			final opencv_core.Size size = input.size();
+			final Mat shadow = new Mat(size, 24, new opencv_core.Scalar(0, 0, 0, 255));
+
+			// make the result in total 16px wider
+			final opencv_core.Size resultSize = new opencv_core.Size(size.width() + 16, size.height() + 16);
+			final Mat result = new Mat(resultSize, 24, new opencv_core.Scalar(0, 0, 0, 0));
+
+			// copy the black shadow over
+			final Mat shadowROI = result.apply(new opencv_core.Rect(8, 8, size.width(), size.height()));
+			shadow.copyTo(shadowROI);
+
+			// blur the shadow
+			final opencv_core.Size kernelSize = new opencv_core.Size(10, 10);
+			blur(result, result, kernelSize);
+
+			// copy in the original image offset by 6,6 topleft
+			final Mat imageROI = result.apply(new opencv_core.Rect(6, 6, size.width(), size.height()));
+			input.copyTo(imageROI);
+
+			// save for comparison sake
+			final File parentFile = inputFile.getParentFile();
+			final String strippedFileName = getFileNameWithoutExtension(inputFile);
+
+			final File outputFile = new File(parentFile, strippedFileName + "-shaded.png");
+			Loader.save(outputFile, result);
 		}
-
-		// copy data over to create a new Black Shadow
-		final opencv_core.Size size = input.size();
-		final Mat shadow = new Mat(size, 24, new opencv_core.Scalar(0, 0, 0, 255));
-
-		// make the result in total 16px wider
-		final opencv_core.Size resultSize = new opencv_core.Size(size.width() + 16, size.height() + 16);
-		final Mat result = new Mat(resultSize, 24, new opencv_core.Scalar(0, 0, 0, 0));
-
-		// copy the black shadow over
-		final Mat shadowROI = result.apply(new opencv_core.Rect(8, 8, size.width(), size.height()));
-		shadow.copyTo(shadowROI);
-
-		// blur the shadow
-		final opencv_core.Size kernelSize = new opencv_core.Size(10, 10);
-		blur(result, result, kernelSize);
-
-		// copy in the original image offset by 6,6 topleft
-		final Mat imageROI = result.apply(new opencv_core.Rect(6, 6, size.width(), size.height()));
-		input.copyTo(imageROI);
-
-		// save for comparison sake
-		final File parentFile = inputFile.getParentFile();
-		final String strippedFileName = getFileNameWithoutExtension(inputFile);
-
-		final File outputFile = new File(parentFile, strippedFileName + "-shaded.png");
-		Loader.save(outputFile, result);
 	}
 
 	private static String getFileNameWithoutExtension(File file) {
